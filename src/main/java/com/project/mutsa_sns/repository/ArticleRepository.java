cf1df6c017705241f6b4e11967fe0d853a8934ca
dothur <craz1000@naver.com>
2023-08-08 21:16:18 +0900

    :bug: fix. 불필요 코드 삭제

--- src/main/java/com/project/mutsa_sns/repository/ArticleRepository.java
+++ src/main/java/com/project/mutsa_sns/repository/ArticleRepository.java
@@ -1,17 +1,8 @@
 package com.project.mutsa_sns.repository;
 
 import com.project.mutsa_sns.entity.ArticleEntity;
-import com.project.mutsa_sns.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
-
-    List<ArticleEntity> findAllByUser(UserEntity user);
-
-    List<ArticleEntity> findAllByDraftFalse();
-
-    List<ArticleEntity> findAllByUserAndDraftFalse(UserEntity user);
-
 }
